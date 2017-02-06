fab1dfae31ec79afdd56222df043f1bba941d89e
raimonfuns <raimonfuns@163.com>
2017-02-06 14:19:10 +0800

    fixed relative localtion

--- src/config.js
+++ src/config.js
@@ -1,5 +1,4 @@
 module.exports = {
 	env: process.env.NODE_ENV || 'test',
-	// api_root: process.env.NODE_ENV == 'production' ? 'http://58.215.180.210:8090' : 'http://localhost:8090'
-	api_root: 'http://localhost:8090'
+	api_root: `http://${window.location.hostname}:8090`
 }
